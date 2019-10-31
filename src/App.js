81650ce828191a514ca0bcf27c5403de05a6ed7a
Vicky Law <vicky.law1217@gmail.com>
2019-10-31 18:36:58 -0400

    show images on page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import React, { Component } from 'react';
+// import ReactDOM from 'react-dom';
 import Header from './components/layout/Header'
+//import imageTest from "./images/totoro1.jpg";
+
+// import ImageDisplay from './components/layout/ImageDisplay'
 import './App.css';
+var listOfImages = [];
 
 class App extends Component {
+  importAll(r) {
+    return r.keys().map(r);
+  }
+  componentWillMount() {
+    listOfImages = this.importAll(
+      require.context("./images/", false, /\.(png|jpg|svg)$/)
+    );
+  }
   render() {
-  return (
-    <div className="App">
-      <Header/>
-    </div>
-  );
+    
+    return (
+      <div className="App">
+        <Header />
+       
+        {listOfImages.map((image, index) => (
+          <img key={index} src={image} alt="info"></img>
+        ))}
+      </div>
+    );
   }
 }
 

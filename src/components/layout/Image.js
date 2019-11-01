758d94a47f08a44e7ea679d1cdb09e67d68a64f1
Vicky Law <vicky.law1217@gmail.com>
2019-11-01 12:59:31 -0400

    images resize

--- src/components/layout/Image.js
+++ src/components/layout/Image.js
@@ -1,34 +1,56 @@
 import React, { Component } from 'react';
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 var listOfImages = [];
 
 class Image extends Component {
-  importAll(r) {
-    return r.keys().map(r);
-  }
-  componentWillMount() {
-    listOfImages = this.importAll(
-      require.context("../../images/", false, /\.(png|jpg|svg)$/)
-    );
-  }
-
-  render() {
-    const imageStyle = {
-      width: 300,
-      height: 300
+    importAll(r) { 
+            return r.keys().map(r);
+        }
+    
+    
+    componentWillMount() {    
+         listOfImages = this.importAll (
+                require.context("../../images/", false, /\.(jpg)$/)   
+         )
     };
+    
 
-    return (
-      <div className="Image">
-      
-        <div className="Container">
-          {listOfImages.map((image, index) => (
-            <img key={index} src={image} alt="info" sytle={imageStyle}></img>
-          ))}
-        </div>
-      </div>
-    );
+  render() {    
+      return (       
+          <Container>
+              <Row>
+                  <Col md={3}>
+            
+                      {listOfImages.map((image, index) => (
+                          <img
+                              key={index}
+                              src={image}
+                              alt=""
+                              width={200}
+                              height={150}
+                          ></img>
+            
+                      ))}
+                  </Col>
+                  <Col md={3}>
+                      {listOfImages.map((image, index) => (
+                          <img
+                              key={index}
+                              src={image}
+                              alt=""
+                              width={200}
+                              height={150}
+                          ></img>
+                      ))}
+                  </Col>
+              </Row>
+          </Container>
+
+      );
   }
 }
 
-export default Image;
+export default Image; 
